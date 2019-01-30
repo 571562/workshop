23ca48ae34da8d599f63913bcdc4cd23b28a2409
571562 <herborgisjo@hotmail.com>
2019-01-30 18:55:37 +0100

    Updates

---
+++ precache-manifest.2e1c778c15d3de4091ee138db0265674.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c0576228a693b5cc923",
+    "url": "/workshop/static/css/main.1bf256d5.chunk.css"
+  },
+  {
+    "revision": "2c0576228a693b5cc923",
+    "url": "/workshop/static/js/main.2c057622.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/workshop/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a66ac1f97ff004b272d2",
+    "url": "/workshop/static/js/runtime~main.a66ac1f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/workshop/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f1c2b8725ed6ced852c3d59a5799ca5",
+    "url": "/workshop/index.html"
+  }
+];
